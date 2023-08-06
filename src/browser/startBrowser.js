62d60ae83a4902ff09457e94f1b37a117f312a3a
Ravil <ravil.nell@gmail.com>
2023-08-06 12:01:27 +0400

    use browserless for production

--- src/browser/startBrowser.js
+++ src/browser/startBrowser.js
@@ -3,9 +3,13 @@ import StealthPlugin from 'puppeteer-extra-plugin-stealth';
 
 puppeteer.use(StealthPlugin());
 
+const IS_PRODUCTION = process.env.NODE_ENV === 'production';
+
 export async function startBrowser() {
   try {
-    const browser = await puppeteer.launch({
+    const browser = IS_PRODUCTION ? await puppeteer.connect({
+      browserWSEndpoint: `wss://chrome.browserless.io?token=${process.env.BROWSERLESS_TOKEN}`
+    }) : await puppeteer.launch({
       headless: true,
     });
     return browser;
