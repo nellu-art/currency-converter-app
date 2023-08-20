cb42854e5a4192a0f91006df03539d0f00d79d6c
Ravil <ravil.nell@gmail.com>
2023-08-20 09:01:32 +0400

    execute updateJob from Monday to Friday

--- src/controllers/setUpdateJob.js
+++ src/controllers/setUpdateJob.js
@@ -4,6 +4,8 @@ import { updateCurrenciesRates } from './currency.controller.js'
 const rule = new schedule.RecurrenceRule();
 // executes the function every hour at 00 minutes after the hour
 rule.minute = 0;
+// executes the function from Monday to Friday
+rule.dayOfWeek = [new schedule.Range(1, 5)];
 
 export function setUpdateJob() {
   schedule.scheduleJob(rule, updateCurrenciesRates);
