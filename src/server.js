65ae0da458555850481cdfc1a184db2734deb198
Ravil <ravil.nell@gmail.com>
2024-11-09 01:46:38 +0100

    fix dotenv

--- src/server.js
+++ src/server.js
@@ -1,5 +1,9 @@
-import 'dotenv/config';
+import { IS_PRODUCTION } from './constants/isProduction.js';
 import { updateCurrenciesRates } from './controllers/updateCurrenciesRates.js';
+
+if (!IS_PRODUCTION) {
+    require('dotenv').config();
+}
 
 const main = async () => {
     console.log('Starting updating currencies rates...');
